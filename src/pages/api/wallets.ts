dcca69fdbc4cc7e4bdbcbb634009231ee7ebaeee
Ben Elferink <ben.elferink@icloud.com>
2024-05-04 16:32:54 +0300

    add token amounts on GET /wallets

--- src/pages/api/wallets.ts
+++ src/pages/api/wallets.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { firestore } from '@/utils/firebase'
+import badLabsApi from '@/utils/badLabsApi'
 
 export const config = {
   maxDuration: 300,
@@ -8,18 +9,36 @@ export const config = {
   },
 }
 
+const TRTL_TOKEN_ID = '52162581184a457fad70470161179c5766f00237d4b67e0f1df1b4e65452544c'
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method, body } = req
+  const { method, query, body } = req
 
   const collection = firestore.collection('turtle-syndicate-wallets')
 
   try {
     switch (method) {
       case 'GET': {
+        const { with_amount: withAmount } = query
+
         const { docs } = await collection.get()
+        const mapped = docs.map((d) => ({ ...d.data(), id: d.id }))
+
+        if (withAmount) {
+          // @ts-ignore
+          for await (const [idx, { cardano }] of mapped.entries()) {
+            const { tokens } = await badLabsApi.wallet.getData(cardano, { withTokens: true })
+
+            const trtl = tokens?.find((t) => t.tokenId === TRTL_TOKEN_ID)
+
+            // @ts-ignore
+            mapped[idx].tokenAmount = trtl?.tokenAmount || null
+          }
+        }
 
         return res.status(200).json({
-          items: docs.map((d) => ({ ...d.data(), id: d.id })),
+          count: mapped.length,
+          items: mapped,
         })
       }
 
@@ -30,6 +49,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
         if (docs.length) {
           return res.status(201).json({
+            count: docs.length,
             items: docs.map((d) => d.id),
           })
         }
@@ -40,6 +60,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         })
 
         return res.status(201).json({
+          count: 1,
           items: [doc.id],
         })
       }
