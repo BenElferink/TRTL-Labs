1d1a464a7f8df80ceea0e80db3afd2af13493866
Ben Elferink <ben.elferink@icloud.com>
2024-10-03 20:16:12 +0300

    fix meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
         <link rel='manifest' href='/manifest.json' />
 
-        <title>TRTL | Solana Bridge</title>
+        <title>TRTL | Labs</title>
       </Head>
 
       <Toaster position='top-right' />
